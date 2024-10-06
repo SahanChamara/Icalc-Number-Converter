870a822acc0eb28436c85df4ab1cad88fe26f08c
SahanChamara <sahanchamara456@gmail.com>
2024-10-06 16:44:02 +0530

    fifth commit

--- icalcnumberconverter.java
+++ icalcnumberconverter.java
@@ -471,9 +471,251 @@ class icalcnumberconverter {
 
 					}
 					break;
+					
+		
+		
+	case 5:		
+	char romannumber='Y';
+			while (romannumber=='Y'){
+				//System.out.print("\033[H\033[2J");  
+				//System.out.flush();
+		
+			System.out.println("\t\t\t\t\t\t\t+-----------------------------------------------+");
+			System.out.println("\t\t\t\t\t\t\t|\t\t Roman Number Converter \t|");
+			System.out.println("\t\t\t\t\t\t\t+-----------------------------------------------+");
+			
+			System.out.println();
+			System.out.println();
+			
+			// hexadecimal CONVERTION =============================================================================
+			
+			System.out.println("\t\t\t\t\t\t\t[01] Decimal Number to Roman Number Converter");
+			System.out.println();
+			System.out.println("\t\t\t\t\t\t\t[02] Roman Number to Decimal Number Converter");
+
+			System.out.print("\t\t\t\tEnter an Option -> ");
+			int optionroman = input.nextInt();
+			System.out.println();
+			
+			switch(optionroman){
+				case 1:
+				System.out.print("\033[H\033[2J");
+				System.out.flush();
+				
+				System.out.println("\t\t\t\t\t\t\t+---------------------------------------------------------+");
+				System.out.println("\t\t\t\t\t\t\t|\t\t Decimal Number to Roman Number Converter |");
+				System.out.println("\t\t\t\t\t\t\t+---------------------------------------------------------+");
+				
+				input.nextLine();
+				System.out.print("\t\t\t\t\t\t\tEnter a Decimal Number : ");
+				String num = input.nextLine();
+				
+				boolean validity = true;
+				
+				if (num.isEmpty()) {
+						validity = false;
+				} else {
+					for (int i = 0; i < num.length(); i++) {
+						char base = num.charAt(i);
+						if (base < '0' || base > '9') {
+							validity = false;
+							break;
+						}
+					}
+				}
+				if (validity) {
+					int number = Integer.parseInt(num);
+					String romanNumber = "";
+
+					while (number >= 1000) {
+						romanNumber += "M";
+						number -= 1000;
+					}
+					while (number >= 900) {
+						romanNumber += "CM";
+						number -= 900;
+					}
+					while (number >= 500) {
+						romanNumber += "D";
+						number -= 500;
+					}
+					while (number >= 400) {
+						romanNumber += "CD";
+						number -= 400;
+					}
+					while (number >= 100) {
+						romanNumber += "C";
+						number -= 100;
+					}
+					while (number >= 90) {
+						romanNumber += "XC";
+						number -= 90;
+					}
+					while (number >= 50) {
+						romanNumber += "L";
+						number -= 50;
+					}
+					while (number >= 40) {
+						romanNumber += "XL";
+						number -= 40;
+					}
+					while (number >= 10) {
+						romanNumber += "X";
+						number -= 10;
+					}
+					while (number >= 9) {
+						romanNumber += "IX";
+						number -= 9;
+					}
+					while (number >= 5) {
+						romanNumber += "V";
+						number -= 5;
+					}
+					while (number >= 4) {
+						romanNumber += "IV";
+						number -= 4;
+					}
+					while (number >= 1) {
+						romanNumber += "I";
+						number -= 1;
+					}
+					
+					System.out.println("\t\t\t\t\t\t\tRoman numeral : " + romanNumber);
+					System.out.println();
+
+					System.out.print("\t\t\t\t\t\t\t\tDo you want to go to home page (Y/N) -> ");
+					homepage = input.next().charAt(0);
+					
+					if (homepage =='Y' || homepage =='y') {
+						romannumber = 'N';
+					} else {
+						return;
+					}
+				} else {
+					System.out.print("\t\t\t\t\t\t\t\tInvalid Input");
+					System.out.println();
+					System.out.print("\t\t\t\t\t\t\t\tDo you want to go previous menu (Y/N) -> ");
+					romannumber = input.next().charAt(0);
+							
+					if (romannumber=='Y' || romannumber=='y') {
+						romannumber = 'Y';
+					} else {
+						return;
+					}
+				}
+				break;	
+				
+				case 2:
+
+					//System.out.print("\033[H\033[2J");  
+					//System.out.flush();
+
+				System.out.println("\t\t\t\t\t\t\t+---------------------------------------------------------+");
+				System.out.println("\t\t\t\t\t\t\t|\t\t Roman Number to Decimal Number Converter |");
+				System.out.println("\t\t\t\t\t\t\t+---------------------------------------------------------+");
+
+					input.nextLine();
+					System.out.print("\t\t\t\t\t\t\t\tEnter a Roman Number : ");
+					String inNum = input.nextLine();
+
+					validity = true;
+
+					for (int i = 0; i < inNum.length(); i++) {
+						char base = inNum.charAt(i);
+						if (base != 'I' && base != 'V' && base != 'X' &&
+							base != 'L' && base != 'C' && base != 'D' &&
+							base != 'M') {
+							validity = false;
+							break;
+						}
+					}
+		
+					if (validity == true){
+					
+						int res = 0;
+						int n = inNum.length();
+		
+						for (int i = 0; i < n; i++) {
+							int s1 = 0;
+							switch (inNum.charAt(i)) {
+								case 'I': s1 = 1; break;
+								case 'V': s1 = 5; break;
+								case 'X': s1 = 10; break;
+								case 'L': s1 = 50; break;
+								case 'C': s1 = 100; break;
+								case 'D': s1 = 500; break;
+								case 'M': s1 = 1000; break;
+							}
+								
+							int s2 = 0;
+							if (i + 1 < n) {
+								switch (inNum.charAt(i + 1)) {
+									case 'I': s2 = 1; break;
+									case 'V': s2 = 5; break;
+									case 'X': s2 = 10; break;
+									case 'L': s2 = 50; break;
+									case 'C': s2 = 100; break;
+									case 'D': s2 = 500; break;
+									case 'M': s2 = 1000; break;
+								}
+							}
+						
+							if (s1 >= s2) {
+								res = res + s1;
+							} else {
+								res = res + s2 - s1;
+								i++;
+							}
+						}
+						System.out.println("\t\t\t\t\t\t\t\tDecimal number : " + res);
+						System.out.println();
+						System.out.print("\t\t\t\t\t\t\t\tDo you want to go to home page (Y/N) -> ");
+						homepage = input.next().charAt(0);
+		
+						if (homepage =='Y' || homepage =='y'){
+							romannumber = 'N';
+						}else{
+							return;
+						}
+					} else {
+						System.out.print("\t\t\t\t\t\t\t\tInvalid Input");
+						System.out.println();
+						System.out.print("\t\t\t\t\t\t\t\tDo you want to go previous menu (Y/N) -> ");
+						romannumber = input.next().charAt(0);
+								
+						if (romannumber=='Y' || romannumber=='y'){
+							romannumber='Y';
+						}else{
+							return;
+						}
+					}
+					break;
+			
+				default:
+					System.out.println("\t\t\t\t\t\t\t\tInput valid option");
+					System.out.println();
+					break;
+			}				
+		}
+		break;
+
+		default:
+			System.out.println("\t\t\t\t\t\t\t\tInput valid option selected");
+			System.out.println();
+			System.out.print("\t\t\t\t\t\t\t\tDo you want to go to home page (Y/N) -> ");
+						homepage = input.next().charAt(0);
+		
+						if (homepage =='Y' || homepage =='y'){
+							romannumber = 'N';
+						}else{
+							return;
+						}
+		break;
+				
+			}
 
 			}
 		}
 
 	}
-}
+
